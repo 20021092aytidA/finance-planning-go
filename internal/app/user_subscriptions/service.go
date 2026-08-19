581f501c0c31be5545b23c48ff829530419915fb
Aditya Cahyadi Sujanadi <aditya.cahyadi.29@gmail.com>
2026-08-19 15:23:36 +0700

    fix: preload not working for user sub GET

--- internal/app/user_subscriptions/service.go
+++ internal/app/user_subscriptions/service.go
@@ -5,7 +5,7 @@ import "finance-planning-go/internal/app/database"
 func (us UserSubscriptionService) Get(query AllowedQuery) (error, []ViewModel) {
 	var userSubs []ViewModel
 	offset := (query.Page - 1) * query.Limit
-	if err := database.DB.Table("user_subscriptions").Preload("Users").Preload("Subscriptions").Where(&query.DataQuery).Offset(offset).Limit(query.Limit).Find(&userSubs).Error; err != nil {
+	if err := database.DB.Table("user_subscriptions").Preload("User").Preload("Subscription").Where(&query.DataQuery).Offset(offset).Limit(query.Limit).Find(&userSubs).Error; err != nil {
 		return err, userSubs
 	}
 
