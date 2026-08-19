d698387f740d7c6dd5c1352e491aec0f6e20c968
Aditya Cahyadi Sujanadi <aditya.cahyadi.29@gmail.com>
2026-08-19 15:20:49 +0700

    fix: wrong url for user sub

--- internal/app/user_subscriptions/route.go
+++ internal/app/user_subscriptions/route.go
@@ -3,5 +3,5 @@ package usersubscriptions
 import "github.com/gin-gonic/gin"
 
 func (us UserSubscriptionRoute) InitRoute(c *gin.RouterGroup) {
-	c.GET("/user_subscriptions", UserSubscriptionHanlder{}.View)
+	c.GET("/user-subscriptions", UserSubscriptionHanlder{}.View)
 }
